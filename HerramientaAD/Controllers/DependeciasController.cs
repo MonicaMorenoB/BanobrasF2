32dd335812101fdb10a10e47f6d8ac9de1093e62
Monica Moreno Barcenas <monica.moreno@stkbanobras.com>
2019-08-14 15:34:16 -0500

    Inicio de Dependencias

---
+++ HerramientaAD/Controllers/DependeciasController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml;
+using HerramientaAD.Models;
+using HerramientaAD.com.Datos;
+using HerramientaAD.com.Utilerias;
+
+
+namespace HerramientaAD.Controllers
+{
+    public class DependeciasController : Controller
+    {
+       DependeciasModel datosDependecias = new DependeciasModel();
+        List<ListasDesplegables> aplicacionesLista = new List<ListasDesplegables>();
+        public ActionResult Index()
+        {
+            var detalleDependeciasModel = new DependeciasModel(int.Parse(Session["UsuarioID"].ToString()));
+            return View(detalleDependeciasModel);
+        }
+        public JsonResult ActualizarAplicaciones(int AreaID)
+        {
+            Aplicaciones aplicaciones = new Aplicaciones();
+
+            if (aplicaciones.AplicacionesConsulta(1, AreaID, 0))
+            {
+                aplicacionesLista.Add(new ListasDesplegables(0, "Selecciona"));
+                XmlNode xmlNode = aplicaciones.ResultadoXML.DocumentElement.SelectSingleNode("Aplicaciones");
+                foreach (XmlNode elemento in xmlNode.SelectNodes("row"))
+                {
+                    aplicacionesLista.Add(new ListasDesplegables(
+                        int.Parse(elemento.Attributes["AplicacionID"].Value.ToString()),
+                        elemento.Attributes["Aplicacion"].Value.ToString())
+                        );
+                }
+            }
+
+            return Json(new SelectList(aplicacionesLista, "Indice", "Texto"), JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult ActualizaDetalle(int appid, string nombre)
+        {
+            DependeciasModel objdep = new DependeciasModel(appid, int.Parse(Session["usuid"].ToString()), nombre);
+            return PartialView("Detalle", objdep);
+        }
+    }
+}
---
+++ HerramientaAD/Models/DependeciasModel.cs
@@ -0,0 +1,82 @@
+﻿using HerramientaAD.com.Datos;
+using HerramientaAD.com.Utilerias;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+
+namespace HerramientaAD.Models
+{
+    public class DependeciasModel
+    {
+        Areas areas = new Areas();
+
+        private List<ListasDesplegables> areasLista = new List<ListasDesplegables>();
+        DatosDetalleTecnico datosDetalle = new DatosDetalleTecnico();
+        public List<ListasDesplegables> AreasLista
+        {
+            get { return areasLista; }
+            set { areasLista = value; }
+        }
+
+        private List<ListasDesplegables> aplicacionesLista = new List<ListasDesplegables>();
+        public List<ListasDesplegables> AplicacionesLista
+        {
+            get { return aplicacionesLista; }
+            set { aplicacionesLista = value; }
+        }
+        private int areaID;
+        public int AreaID
+        {
+            get { return areaID; }
+            set { areaID = value; }
+        }
+
+        private int aplicacionID;
+        public int AplicacionID
+        {
+            get { return aplicacionID; }
+            set { aplicacionID = value; }
+        }
+
+        private XmlDocument detalleXML;
+        public XmlDocument DetalleXML
+        {
+            get { return detalleXML; }
+            set { detalleXML = value; }
+        }
+
+        public DependeciasModel()
+        {
+
+        }
+        public DependeciasModel(int UsuarioID)
+        {
+            if (areas.AreasConsulta(UsuarioID))
+            {
+                XmlNode xmlNode = areas.ResultadoXML.DocumentElement.SelectSingleNode("Areas");
+                foreach (XmlNode elemento in xmlNode.SelectNodes("row"))
+                {
+                    areasLista.Add(new ListasDesplegables(
+                        int.Parse(elemento.Attributes["AreaID"].Value.ToString()),
+                        elemento.Attributes["Descripcion"].Value.ToString()));
+                }
+            }
+
+            if (datosDetalle.ConsultaDetalleFiltro("BD", 0, string.Empty, string.Empty, string.Empty, string.Empty))
+            {
+                detalleXML = datosDetalle.ResultadoXML;
+            }
+        }
+
+        public DependeciasModel(int appid, int usuarioid, string nombre)
+        {
+            //Falta el SP
+            //Aplicaciones objapp = new Aplicaciones();
+            //objapp.ObtenObjetosDB3(usuarioid, appid, nombre);
+            //XobjetosDB = objapp.AplicaionXML;
+        }
+
+    }
+}
