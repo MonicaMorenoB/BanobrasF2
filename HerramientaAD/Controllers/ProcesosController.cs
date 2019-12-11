aeb9a6699700a90ec3bf0ebb2239cdbd8d42a7c0
Monica Moreno Barcenas <monica.moreno@stkbanobras.com>
2019-12-11 10:00:43 -0600

    Se agregó pestaña para la parte de procesos

---
+++ HerramientaAD/Controllers/ProcesosController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using System.Windows;
+
+namespace HerramientaAD.Controllers
+{
+    public class ProcesosController : Controller
+    {
+        // GET: Procesos
+        public ActionResult Index()
+        {
+            return RedirectToAction("Index", "Procesos");
+        }
+    }
+}
